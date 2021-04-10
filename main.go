ce5f2e9847ec8f60b3278cb515e94943663da8e0
Hansin1997 <845612500@qq.com>
2021-04-10 22:36:28 +0800

    使用协程和waitGroup

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/url"
 	"strings"
+	"sync"
 )
 
 func initBloomFilter(config Config) BloomFilter {
@@ -17,9 +18,17 @@ func initFetcher(config Config) Fetcher {
 	return newDefaultFetcher(config.Accepts)
 }
 
-func initQueue(config Config) (Queue, error) {
+func initQueue(config Config) ([]Queue, error) {
 	log.Println("[Init Queue]")
-	return newRbQueue(config.RabbitMq.Url, config.RabbitMq.Exchange, config.RabbitMq.Queue, config.RabbitMq.RoutingKey)
+	queues := make([]Queue, config.Workers)
+	for i := 0; i < config.Workers; i++ {
+		queue, err := newRbQueue(config.RabbitMq.Url, config.RabbitMq.Exchange, config.RabbitMq.Queue, config.RabbitMq.RoutingKey)
+		if err != nil {
+			return nil, err
+		}
+		queues[i] = queue
+	}
+	return queues, nil
 }
 
 func initStorage(config Config) (Storage, error) {
@@ -80,8 +89,9 @@ func pushSeeds(filter BloomFilter, queue Queue, seeds []string) error {
 			log.Println(err)
 			continue
 		}
-		seed = computeUrl(u)
-		exists, err := filter.Exists(seed)
+		key := computeKey(u)
+		seed = u.Scheme + "://" + key
+		exists, err := filter.Exists(key)
 		if err != nil {
 			return err
 		}
@@ -90,7 +100,7 @@ func pushSeeds(filter BloomFilter, queue Queue, seeds []string) error {
 			if err != nil {
 				return err
 			}
-			_, err = filter.Add(seed)
+			_, err = filter.Add(key)
 			if err != nil {
 				log.Println(err)
 			}
@@ -102,13 +112,67 @@ func pushSeeds(filter BloomFilter, queue Queue, seeds []string) error {
 	return nil
 }
 
+func handle(content string, filter BloomFilter, fetcher Fetcher, storage Storage, queue Queue) (bool, error) {
+	u, err := url.Parse(content)
+
+	if err != nil {
+		log.Println(err)
+		return true, nil
+	}
+	target := computeUrl(u)
+	doc, urls, success, err := fetcher.Fetch(target)
+	if err != nil {
+		log.Println(err)
+		return true, nil
+	}
+	if !success {
+		return true, nil
+	}
+	err = storage.Save(doc)
+	if err != nil {
+		log.Println(err)
+		return false, nil
+	}
+	log.Printf("[Save] %s [%s]\n", target, doc.Title)
+	for _, newUrl := range urls {
+		newUrl = strings.TrimSpace(newUrl)
+		if newUrl == "" || strings.HasPrefix(newUrl, "#") || strings.HasPrefix(newUrl, "javascript") {
+			continue
+		}
+		nu, err := url.Parse(newUrl)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		r := u.ResolveReference(nu)
+		newKey := computeKey(r)
+		newUrl = r.Scheme + "://" + newKey
+		exists, err := filter.Exists(newKey) // 检查是否存在
+		if err != nil {
+			return false, err
+		}
+		if exists {
+			continue
+		}
+		err = queue.Publish(newUrl) // 入队
+		if err != nil {
+			return false, nil
+		}
+		filter.Add(newKey) // 更新过滤器
+	}
+	return true, nil
+}
+
 func main() {
 
 	cfg := loadConfig("config.json") // 载入配置
-	_reset := cfg.Reset              // 是否重置
-	filter := initBloomFilter(cfg)   // 初始化布隆过滤器
+	if cfg.Workers < 1 {
+		cfg.Workers = 1
+	}
+	_reset := cfg.Reset            // 是否重置
+	filter := initBloomFilter(cfg) // 初始化布隆过滤器
 	fetcher := initFetcher(cfg)
-	queue, err := initQueue(cfg)
+	queues, err := initQueue(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -118,67 +182,31 @@ func main() {
 	}
 
 	if _reset {
-		reset(filter, queue, storage) // 重置
+		reset(filter, queues[0], storage) // 重置
 	}
 
-	err = pushSeeds(filter, queue, cfg.Seeds)
+	err = pushSeeds(filter, queues[0], cfg.Seeds)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	log.Println("")
 	log.Println("[Start]")
-	queue.Consume(func(content string) (bool, error) {
 
-		u, err := url.Parse(content)
-
-		if err != nil {
-			log.Println(err)
-			return true, nil
-		}
-		target := computeUrl(u)
-		doc, urls, success, err := fetcher.Fetch(target)
-		if err != nil {
-			log.Println(err)
-			return true, nil
-		}
-		if !success {
-			return true, nil
-		}
-		err = storage.Save(doc)
-		if err != nil {
-			log.Println(err)
-			return false, nil
-		}
-		log.Printf("[Save] %s [%s]\n", target, doc.Title)
-		for _, newUrl := range urls {
-			newUrl = strings.TrimSpace(newUrl)
-			if newUrl == "" || strings.HasPrefix(newUrl, "#") || strings.HasPrefix(newUrl, "javascript") {
-				continue
-			}
-			nu, err := url.Parse(newUrl)
+	wg := sync.WaitGroup{}
+	for _, queue := range queues {
+		wg.Add(1)
+		go func(queue Queue) {
+			err := queue.Consume(func(content string) (bool, error) {
+				return handle(content, filter, fetcher, storage, queue)
+			})
 			if err != nil {
-				log.Println(err)
-				continue
+				log.Fatal(err)
+			} else {
+				wg.Done()
 			}
-			r := u.ResolveReference(nu)
-			newKey := computeKey(r)
-			newUrl = r.Scheme + "://" + newKey
-			exists, err := filter.Exists(newKey) // 检查是否存在
-			if err != nil {
-				return false, err
-			}
-			if exists {
-				continue
-			}
-			err = queue.Publish(newUrl) // 入队
-			if err != nil {
-				return false, nil
-			}
-			filter.Add(newKey) // 更新过滤器
-		}
-		return true, nil
-	})
-
+		}(queue)
+	}
+	wg.Wait()
 	log.Println("[Finished]")
 }
